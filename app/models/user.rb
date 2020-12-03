9b6af7da91254cf638a020879c9078e87ef120ae
Vitalii Perehonchuk <brute18@gmail.com>
2020-12-03 19:31:40 +0200

    inverse_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :chats, join_table: 'chats_users'
   has_many :bots, foreign_key: :owner_id
-  has_many :messages, foreign_key: :author_id
+  has_many :messages, inverse_of: :author
   validates :role, inclusion: { in: %w[admin], allow_null: true }
 
   def admin?
